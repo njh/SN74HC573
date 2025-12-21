f7764c6539e54b1d3db8612a0488473b2d6038de
KITTEN2008 <elt.sans@bk.ru>
2025-12-21 18:02:40 +0300

    Update SN74HC573.h

--- src/SN74HC573.h
+++ src/SN74HC573.h
@@ -25,13 +25,39 @@ class SN74HC573 {
     void toggle(uint8_t pin);
     bool read(uint8_t pin);
     
+    // Работа с портами
+    void writePort(uint8_t portValue);
+    uint8_t readPort();
+    
     // Режимы работы
     enum Mode {
       MODE_NORMAL,
       MODE_TRANSPARENT,
       MODE_LATCHED
     };
+    
     void setMode(uint8_t mode);
+    void setInversion(bool invert);
+    
+    // Расширенные функции
+    void pulse(uint8_t value, uint16_t duration);
+    void shiftLeft(bool fillZero = true);
+    void shiftRight(bool fillZero = true);
+    void rotateLeft();
+    void rotateRight();
+    
+    // Битовые маски
+    enum {
+      BIT0 = 0x01,
+      BIT1 = 0x02,
+      BIT2 = 0x04,
+      BIT3 = 0x08,
+      BIT4 = 0x10,
+      BIT5 = 0x20,
+      BIT6 = 0x40,
+      BIT7 = 0x80,
+      ALL_BITS = 0xFF
+    };
     
   private:
     uint8_t _dataPins[8];
@@ -39,6 +65,9 @@ class SN74HC573 {
     uint8_t _oePin;
     uint8_t _currentValue;
     uint8_t _mode;
+    bool _invertOutputs;
+    bool _outputsEnabled;
+    
     void _writeToPins(uint8_t value);
     void _updateLatch();
 };
